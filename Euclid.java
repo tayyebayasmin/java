75119706291cd7b9c9856049914e6cb433ee9baa
tayyebayasmin <tayyebajasmin@gmail.com>
2020-11-23 00:49:40 +0500

    update

--- Euclid.java
+++ Euclid.java
@@ -1,22 +1,45 @@
 
-package euclid;
-import java.util.Scanner;
+/******************************************************************************
+ *  Compilation:  javac Euclid.java
+ *  Execution:    java Euclid p q
+ *  
+ *  Reads two command-line arguments p and q and computes the greatest
+ *  common divisor of p and q using Euclid's algorithm.
+ *
+ *  Remarks
+ *  -----------
+ *    - may return the negative of the gcd if either p or q is negative
+ *
+ ******************************************************************************/
+
 public class Euclid {
-           public static int gcd(int p,int q)
-    {
-        if(q==0)
-            return p;
-        else return gcd(q,p%q);
+
+    // recursive implementation
+    public static int gcd(int p, int q) {
+        if (q == 0) return p;
+        else return gcd(q, p % q);
+
     }
+
+    // non-recursive implementation
+    public static int gcd2(int p, int q) {
+        while (q != 0) {
+            int temp = q;
+            q = p % q;
+            p = temp;
+        }
+        return p;
+    }
+    
+    // main method
     public static void main(String[] args) {
-      Scanner obj=new Scanner(System.in); 
-      System.out.println("enter 2 numbers:");
-      int p=obj.nextInt();
-      int q=obj.nextInt();
-      int d;
-       d = gcd(p ,q);
-       System.out.println("gcd("+ p +","+ q +")="+d);
-       
+        int p = Integer.parseInt(args[0]);
+        int q = Integer.parseInt(args[1]);
+        int d  = gcd(p, q); //resursion
+        int d2 = gcd2(p, q); //while loop
+        System.out.println("gcd(" + p + ", " + q + ") = " + d);
+        System.out.println("gcd(" + p + ", " + q + ") = " + d2);
     }
-    }
-   
+}
+
+
--- HelloWorld.java
+++ HelloWorld.java
@@ -1,8 +1,9 @@
-
-package helloworld;
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("HelloWorld!");
-    }
-    
+
+   public static void main(String[] args) {
+
+       System.out.println("Hello World!");
+
+   }
+
 }
